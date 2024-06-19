e80f991145e7ae724ceb378e11452e525981041b
Lennox Mountain <lennoxmountain67@gmail.com>
2024-06-19 19:38:23 +0200

    added lusitana fonts with its corresponding weights

--- nextjs-dashboard/app/ui/fonts.ts
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -1,3 +1,10 @@
-import { Inter } from 'next/font/google';
- 
+import { Inter, Lusitana} from 'next/font/google';
+
+//exporting Inter font for use
 export const inter = Inter({ subsets: ['latin'] });
+
+//exercise. exporting Lusitana font for use
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+})
