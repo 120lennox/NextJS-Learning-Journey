52424cd9ba2cf921bd3fca99812d0300f521ee07
Lennox Mountain <lennoxmountain67@gmail.com>
2024-06-25 09:34:39 +0200

    have then entered the records into the database

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import {z} from 'zod'  //libray for validating typescript datatypes
+import { sql } from '@vercel/postgres';
 
 const FormSchema = z.object({
     id: z.string(),
@@ -20,4 +21,16 @@ export async function createInvoice(formData: FormData) {
         amount: formData.get('amount'),
         status: formData.get('status')
     })
+
+    //storing monetary values in cents. 
+    const amountInCents = Math.round(parseFloat(amount) * 100)
+
+    //creating new date
+    const date = new Date().toISOString().split('T')[0];
+
+    //entering the records into the database
+    await sql 
+                `INSERT INTO invoices (customer_id, amount, status, date)
+                VALUES (${customerID}, ${amountInCents}, ${status}, ${date})`
+  
 }
