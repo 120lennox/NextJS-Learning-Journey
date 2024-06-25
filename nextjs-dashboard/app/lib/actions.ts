bf5520502862937442640a81d9457f8bb88dcbb3
Lennox Mountain <lennoxmountain67@gmail.com>
2024-06-25 09:21:37 +0200

    validated data types using zod library

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -1,13 +1,23 @@
 'use server';
 
+import {z} from 'zod'  //libray for validating typescript datatypes
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerID: z.string(),
+    amount: z.coerce.string(), // coerce means it will change say from number to string during data transfer
+    status: z.enum(['pending', 'paid']),
+    date: z.string()
+})
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
 
 export async function createInvoice(formData: FormData) {
-    //extracting data from form data
-    const rawFormData = {
+    const {customerID, amount, status} = FormSchema.parse({
+        //extracting data from form data
         customerID: formData.get('customerID'),
         amount: formData.get('amount'),
         status: formData.get('status')
-    } 
-
-    console.log(rawFormData)
+    })
 }
