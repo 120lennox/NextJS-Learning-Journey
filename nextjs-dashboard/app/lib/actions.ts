b4c62a5c4197a48bdeb10a6ede66632af9f85ac3
Lennox Mountain <lennoxmountain67@gmail.com>
2024-06-25 08:56:21 +0200

    extracted form data. More like updating form data. and printed it on console

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -1,11 +1,13 @@
 'use server';
- 
+
+
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
-  };
-  // Test it out:
-  console.log(rawFormData);
+    //extracting data from form data
+    const rawFormData = {
+        customerID: formData.get('customerID'),
+        amount: formData.get('amount'),
+        status: formData.get('status')
+    } 
+
+    console.log(rawFormData)
 }
